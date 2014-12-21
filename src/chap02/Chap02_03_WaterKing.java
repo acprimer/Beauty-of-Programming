e8dabb4f4923dd612cffe0f3a0275b67d38cba23
Dahai Yao <729101421@qq.com>
2014-12-21 21:35:56 +0800

    chap2.3

--- src/chap02/Chap02_03_WaterKing.java
+++ src/chap02/Chap02_03_WaterKing.java
@@ -1,6 +1,8 @@
 package chap02;
 
+import java.util.ArrayList;
 import java.util.Stack;
+
 /**
  * Created by yaodh on 2014/12/20.
  */
@@ -8,6 +10,7 @@ public class Chap02_03_WaterKing {
     /**
      * 给一个ID数组，其中一个ID超过数组长度的一半，求出这个ID。
      * Time: O(n)
+     *
      * @param ids ID数组
      * @return 超过数组长度一般的ID
      */
@@ -24,29 +27,119 @@ public class Chap02_03_WaterKing {
         }
         return candidate;
     }
+
     public int findMostStack(int[] ids) {
         Stack<Integer> stack = new Stack<Integer>();
-        for(int i=0;i<ids.length;i++) {
-            if(stack.isEmpty()) {
+        for (int i = 0; i < ids.length; i++) {
+            if (stack.isEmpty()) {
                 stack.push(ids[i]);
             } else {
-                if(stack.peek()==ids[i]) stack.push(ids[i]);
+                if (stack.peek() == ids[i]) stack.push(ids[i]);
                 else stack.pop();
             }
         }
         return stack.peek();
     }
 
-    public int find4thMost(int[] ids) {
-        int[] candidates = new int[3];
-        int[] count = new int[3];
-        for(int i=0;i<ids.length;i++) {
+    public int find3thMost(int[] ids) {
+        int[] candidates = new int[2];
+        int[] count = new int[2];
+        for (int i = 0; i < ids.length; i++) {
+            if (count[0] == 0) {
+                candidates[0] = ids[i];
+                count[0]++;
+            } else if (ids[i] == candidates[0]) {
+                count[0]++;
+            } else if (count[1] == 0) {
+                candidates[1] = ids[i];
+                count[1]++;
+            } else if (ids[i] == candidates[1]) {
+                count[1]++;
+            } else {
+                count[0]--;
+                count[1]--;
+            }
         }
         return 0;
     }
 
+    public int majorityNumber(ArrayList<Integer> nums) {
+        int[] candidates = new int[2];
+        int[] count = new int[2];
+        for (int i = 0; i < nums.size(); i++) {
+            int cur = nums.get(i), j;
+            for (j = 0; j < 2; j++) {
+                if (count[j] == 0) {
+                    candidates[j] = cur;
+                    count[j]++;
+                    break;
+                } else if (candidates[j] == cur) {
+                    count[j]++;
+                    break;
+                }
+            }
+            if (j >= 2) {
+                count[0]--;
+                count[1]--;
+            }
+        }
+        count[0] = count[1] = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) == candidates[0]) {
+                count[0]++;
+            } else if (nums.get(i) == candidates[1]) {
+                count[1]++;
+            }
+        }
+        if (count[0] > count[1]) return candidates[0];
+        else return candidates[1];
+    }
+
+    public int majorityNumber(ArrayList<Integer> nums, int k) {
+        int[] candidates = new int[k];
+        int[] count = new int[k];
+        for (int i = 0; i < nums.size(); i++) {
+            int cur = nums.get(i), j;
+            for (j = 0; j < k; j++) {
+                if (count[j] == 0) {
+                    candidates[j] = cur;
+                    count[j]++;
+                    break;
+                } else if (candidates[j] == cur) {
+                    count[j]++;
+                    break;
+                }
+            }
+            if (j >= k) {
+                for(int idx=0;idx<k;idx++) {
+                    count[idx]--;
+                }
+            }
+        }
+        for(int j=0;j<k;j++) {
+            count[j]=0;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            for(int j=0;j<k;j++) {
+                if(candidates[j]==nums.get(i)) {
+                    count[j]++;
+                    if(count[j]>nums.size()/k) {
+                        return candidates[j];
+                    }
+                    break;
+                }
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
-        int ans = new Chap02_03_WaterKing().findMost(new int[]{1, 2, 3, 4});
+        int[] a = new int[]{7,3,1,4,5,6,17,8,9,1};
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        for(int i=0;i<a.length;i++) {
+            nums.add(a[i]);
+        }
+        int ans = new Chap02_03_WaterKing().majorityNumber(nums, 10);
         System.out.println(ans);
     }
 }

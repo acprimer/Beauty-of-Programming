a166cf3fe6707dc42562926bdee1c25eda706f72
Dahai Yao <729101421@qq.com>
2015-01-02 16:06:43 +0800

    chap2.21

--- src/chap02/Chap02_21_Add.java
+++ src/chap02/Chap02_21_Add.java
@@ -1,28 +1,33 @@
 package chap02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by yaodh on 2015/1/2.
  */
 public class Chap02_21_Add {
-    int output(int number) {
-        int left = 1, count = 0;
-        for (int i = 2; i < Math.sqrt(2 * number); i++) {
+    List<List<Long>> output(long number) {
+        long left = 1, count = 0;
+        List<List<Long>> ans = new ArrayList<List<Long>>();
+        for (long i = 2; i < Math.sqrt(2) * Math.sqrt(number); i++) {
             if ((number - left) % i == 0) {
                 count++;
-                for (int j = (number - left) / i; j < (number - left) / i + i; j++) {
-                    System.out.printf("%d ", j);
-                }
-                System.out.println();
+//                List<Long> list = new ArrayList<Long>();
+//                for (long j = (number - left) / i; j < (number - left) / i + i; j++) {
+//                    list.add(j);
+//                }
+//                ans.add(list);
             }
             left += i;
         }
-        return count;
+        System.out.println(count);
+        return ans;
     }
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            System.out.println(i + ": ");
-            new Chap02_21_Add().output(i);
-        }
+        long number = (long) 3 * 5 * 7 * 11 * 13 * 17 * 19 * 23 * 29 * 31 * 37 * 41 * 47 * 25 * 27;
+        System.out.println(Long.MAX_VALUE/number);
+        System.out.println(number + ": " + new Chap02_21_Add().output(number).size());
     }
 }

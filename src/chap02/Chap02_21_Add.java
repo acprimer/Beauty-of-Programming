50beddbbb29f1a6679085aa14c7e367df6e5ecb8
Dahai Yao <729101421@qq.com>
2015-01-03 13:00:26 +0800

    chap2.21

--- src/chap02/Chap02_21_Add.java
+++ src/chap02/Chap02_21_Add.java
@@ -7,17 +7,18 @@ import java.util.List;
  * Created by yaodh on 2015/1/2.
  */
 public class Chap02_21_Add {
-    List<List<Long>> output(long number) {
+    List<List<Long>> sum(long number) {
         long left = 1, count = 0;
         List<List<Long>> ans = new ArrayList<List<Long>>();
         for (long i = 2; i < Math.sqrt(2) * Math.sqrt(number); i++) {
             if ((number - left) % i == 0) {
                 count++;
-//                List<Long> list = new ArrayList<Long>();
-//                for (long j = (number - left) / i; j < (number - left) / i + i; j++) {
-//                    list.add(j);
-//                }
-//                ans.add(list);
+                List<Long> list = new ArrayList<Long>();
+                for (long j = (number - left) / i; j < (number - left) / i + i; j++) {
+                    list.add(j);
+                    System.out.println(j);
+                }
+                ans.add(list);
             }
             left += i;
         }
@@ -25,9 +26,40 @@ public class Chap02_21_Add {
         return ans;
     }
 
+    // O(sqrt(n))
+    int sumCount(long number) {
+        long left = 1;
+        int count = 0;
+        for (long i = 2; i < Math.sqrt(2) * Math.sqrt(number); i++) {
+            if ((number - left) % i == 0) {
+                count++;
+            }
+            left += i;
+        }
+        return count;
+    }
+
+    // O(n)
+    int sumCount_2(long target) {
+        long sum = 0;
+        int count = 0;
+        for (long start = 1, end = 1; start <= (target + 1) / 2; end++) {
+            sum += end;
+            if (sum == target) {
+                count++;
+            }
+            while (sum >= target - end) {
+                sum -= start;
+                start++;
+            }
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
-        long number = (long) 3 * 5 * 7 * 11 * 13 * 17 * 19 * 23 * 29 * 31 * 37 * 41 * 47 * 25 * 27;
-        System.out.println(Long.MAX_VALUE/number);
-        System.out.println(number + ": " + new Chap02_21_Add().output(number).size());
+//        long number = (long) 3 * 5 * 7 * 11 * 13 * 17 * 19 * 23 * 29 * 31 * 37 * 41 * 47 * 25 * 27;
+        long number = 987654323;
+        System.out.println(Long.MAX_VALUE / number);
+        System.out.println(number + ": " + new Chap02_21_Add().sumCount_2(number));
     }
 }

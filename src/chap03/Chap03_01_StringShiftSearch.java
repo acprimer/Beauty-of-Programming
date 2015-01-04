87bd95a71f1e8a47250d8a98c0979ad2add6febc
Dahai Yao <729101421@qq.com>
2015-01-04 14:38:57 +0800

    chap3.2

--- src/chap03/Chap03_01_StringShiftSearch.java
+++ src/chap03/Chap03_01_StringShiftSearch.java
@@ -9,11 +9,46 @@ public class Chap03_01_StringShiftSearch {
             return false;
         }
         source += source;
-        return source.contains(target);
+        // 也可以直接调用 return source.contains(target);
+        return search(source, target);
+    }
+
+    boolean search(String source, String target) {
+        if (source.length() < target.length()) {
+            return false;
+        }
+        int j = 0;
+        for (int i = 0; i < source.length() && j < target.length(); i++) {
+            if (source.charAt(i) == target.charAt(j)) {
+                j++;
+            } else {
+                i -= j;
+                j = 0;
+            }
+        }
+        return j >= target.length();
+    }
+
+    boolean shiftSearch_2(String source, String target) {
+        if (source.length() < target.length()) {
+            return false;
+        }
+        int sLen = source.length();
+        int tLen = target.length();
+        int j = 0;
+        for (int i = 0; i < sLen * 2 && j < tLen; i++) {
+            if (source.charAt(i % sLen) == target.charAt(j)) {
+                j++;
+            } else {
+                i -= j;
+                j = 0;
+            }
+        }
+        return j >= tLen;
     }
 
     public static void main(String[] args) {
-        boolean ans = new Chap03_01_StringShiftSearch().shiftSearch("aabcd", "cdaa");
+        boolean ans = new Chap03_01_StringShiftSearch().shiftSearch_2("aabcd", "cdaab");
         System.out.println(ans);
     }
 }
---
+++ src/chap03/Chap03_02_PhoneNumber.java
@@ -0,0 +1,55 @@
+package chap03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yaodh on 2015/1/4.
+ */
+public class Chap03_02_PhoneNumber {
+    public static final String[] maps = {"", "", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"};
+
+    List<String> findWords(String phoneNumber) {
+        List<String> words = new ArrayList<String>();
+        words.add("");
+        words = dfs(words, phoneNumber, 0);
+        return words;
+    }
+
+    private List<String> dfs(List<String> words, String phoneNumber, int start) {
+        if (start >= phoneNumber.length()) {
+            return words;
+        }
+        List<String> list = new ArrayList<String>();
+        int number = phoneNumber.charAt(start) - '0';
+        for (String word : words) {
+            for (int j = 0; j < maps[number].length(); j++) {
+                list.add(word + maps[number].charAt(j));
+            }
+        }
+        return dfs(list, phoneNumber, start + 1);
+    }
+
+    List<String> findWords_2(String phoneNumber) {
+        List<String> words = new ArrayList<String>();
+        words.add("");
+        for (int i = 0; i < phoneNumber.length(); i++) {
+            List<String> list = new ArrayList<String>();
+            for (String word : words) {
+                int number = phoneNumber.charAt(i) - '0';
+                for (int j = 0; j < maps[number].length(); j++) {
+                    list.add(word + maps[number].charAt(j));
+                }
+            }
+            words = list;
+        }
+        return words;
+    }
+
+    public static void main(String[] args) {
+        List<String> ans = new Chap03_02_PhoneNumber().findWords_2("45");
+        for (String word : ans) {
+            System.out.println(word);
+        }
+    }
+}
